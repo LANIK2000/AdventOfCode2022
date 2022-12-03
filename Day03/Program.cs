68d7411f7258aefa63e4547fc6705c41c7f2142d
LANIK2000 <lanikmail@gmail.com>
2022-12-03 19:34:46 +0100

    Day 3 Part 1 DONE!

---
+++ Day03/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int getPriority(char c) {
+	if (c < 'a')
+		return c - 'A' + 27;
+	else
+		return c - 'a' + 1;
+}
+
+int priorities_sum = 0;
+
+string? line;
+
+string compartment_1;
+string compartment_2;
+
+while ((line = Console.ReadLine()) != null) {
+	int half_point = line.Length / 2;
+	compartment_1 = line.Substring(0, half_point);
+	compartment_2 = line.Substring(half_point);
+	
+	var common_items = compartment_1.Intersect(compartment_2);
+	foreach (var item in common_items)
+		priorities_sum += getPriority(item);
+}
+
+Console.WriteLine($"Part one: {priorities_sum}");
