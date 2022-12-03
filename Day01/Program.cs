035ceeecde4faa899c44738c66692cd5a6e429d5
LANIK2000 <lanikmail@gmail.com>
2022-12-03 18:34:48 +0100

    Day 1 complete!

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿string? line;
+
+uint weight = 0;
+List<uint> elves = new();
+
+while ((line = Console.ReadLine()) != null) {
+	if (line == "") {
+		elves.Add(weight);
+		weight = 0;
+	}
+	else
+		weight += uint.Parse(line);
+}
+
+var top_elves = elves.OrderByDescending(x => x).ToArray();
+
+Console.WriteLine($"Part one: {top_elves[0]}");
+Console.WriteLine($"Part two: {top_elves[0] + top_elves[1] + top_elves[2]}");
