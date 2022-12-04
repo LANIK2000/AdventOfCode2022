38a2417832cef5fbfbe06de8cdd3a006ea855ae0
LANIK2000 <lanikmail@gmail.com>
2022-12-04 19:10:12 +0100

    Day 4 Done!

---
+++ Day04/Program.cs
@@ -0,0 +1,32 @@
+﻿
+bool rangeOverlapComplete(ReadOnlySpan<int> values) {
+	var res =
+		Math.Sign(values[0] - values[2])
+		+
+		Math.Sign(values[1] - values[3]);
+	
+	return Math.Abs(res) < 2;
+}
+
+bool rangeOverlapPartial(ReadOnlySpan<int> values)
+	=> values[1] >= values[2] && values[3] >= values[0];
+
+var count_part_1 = 0;
+var count_part_2 = 0;
+
+string? line;
+while ((line = Console.ReadLine()) != null) {
+	var values = line
+		.Split(new[]{',', '-'})
+		.Select(x => int.Parse(x))
+		.ToArray().AsSpan();
+	
+	if (rangeOverlapComplete(values))
+		count_part_1++;
+	
+	if (rangeOverlapPartial(values))
+		count_part_2++;
+}
+
+Console.WriteLine($"Part one: {count_part_1}");
+Console.WriteLine($"Part two: {count_part_2}");
