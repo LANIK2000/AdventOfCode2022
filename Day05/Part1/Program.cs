eb40fed8239ee92b54e7b6452f1ff93c79bba928
LANIK2000 <lanikmail@gmail.com>
2022-12-05 19:02:40 +0100

    Day 5 DONE!

---
+++ Day05/Part1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+/* Init stacks */
+var stacks = new LinkedList<char>[9];
+for (int i = 0; i < 9; i++)
+	stacks[i] = new();
+
+string? line;
+
+{/* Fill stacks */
+	var regex = new Regex(@"\[\w\]");
+
+	while ((line = Console.ReadLine()) != null && line[0] != ' ')
+		foreach (Match match in regex.Matches(line))
+			stacks[match.Index / 4].AddLast(match.ToString()[1]);
+}
+
+Console.ReadLine(); // Skip empty line
+
+{/* Move crates around */
+	var regex = new Regex(@"\d+");
+
+	while ((line = Console.ReadLine()) != null) {
+		var matches = regex.Matches(line);
+		var amount  = int.Parse(matches[0].ToString());
+		var from    = int.Parse(matches[1].ToString()) - 1;
+		var to      = int.Parse(matches[2].ToString()) - 1;
+		
+		for (int i = 0; i < amount; i++) {
+			stacks[to].AddFirst(stacks[from].First());
+			stacks[from].RemoveFirst();
+		}
+	}
+}
+
+Console.WriteLine($"Part one: {string.Join("", stacks.Select(x => x.First()))}");
---
+++ Day05/Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+/* Init stacks */
+var stacks = new LinkedList<char>[9];
+for (int i = 0; i < 9; i++)
+	stacks[i] = new();
+
+string? line;
+
+{/* Fill stacks */
+	var regex = new Regex(@"\[\w\]");
+
+	while ((line = Console.ReadLine()) != null && line[0] != ' ')
+		foreach (Match match in regex.Matches(line))
+			stacks[match.Index / 4].AddLast(match.ToString()[1]);
+}
+
+Console.ReadLine(); // Skip empty line
+
+{/* Move crates around */
+	var regex = new Regex(@"\d+");
+
+	while ((line = Console.ReadLine()) != null) {
+		var matches = regex.Matches(line);
+		var amount  = int.Parse(matches[0].ToString());
+		var from    = int.Parse(matches[1].ToString()) - 1;
+		var to      = int.Parse(matches[2].ToString()) - 1;
+		
+		foreach (var crate in stacks[from].Take(amount).Reverse()) {
+			stacks[to].AddFirst(crate);
+			stacks[from].RemoveFirst();
+		}
+	}
+}
+
+Console.WriteLine($"Part two: {string.Join("", stacks.Select(x => x.First()))}");
