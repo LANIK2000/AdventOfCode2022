df3487a171df3794370adedf54a6cdee415f9362
LANIK2000 <lanikmail@gmail.com>
2022-12-06 23:39:50 +0100

    Day 6 DONE!

---
+++ Day06/Program.cs
@@ -0,0 +1,22 @@
+﻿int IndexOfUniqueSequance(string line, int length) {
+	string buffer = "";
+
+	for (int i = 0; i < line.Length; i++) {
+		char c = line[i];
+		
+		int found = buffer.IndexOf(c);
+		if (found != -1)
+			buffer = buffer.Substring(found + 1);
+		
+		buffer += c;
+		if (buffer.Length == length)
+			return i + 1;
+	}
+	return -1;
+}
+
+string line = Console.ReadLine()
+	?? throw new ArgumentException("No line was given.");
+
+Console.WriteLine($"Part one: {IndexOfUniqueSequance(line, 4)}");
+Console.WriteLine($"Part two: {IndexOfUniqueSequance(line, 14)}");
