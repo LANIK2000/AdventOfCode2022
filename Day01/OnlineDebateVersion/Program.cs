4d93872d732cd6963a6a29212e22cdd48258366f
LANIK2000 <lanikmail@gmail.com>
2022-12-10 21:39:24 +0100

    Remade day 1 for an online argument.

---
+++ Day01/OnlineDebateVersion/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+// This program counts how many calories each elf carries
+// and prints how much the top 1 carries and how much
+// the top 3 carry.
+class Program
+{
+	static void Main()
+	{
+		int total_calories = 0; // Total calories of a single elf
+		List<int> all_elves = new();
+		
+		while (true)
+		{
+			string? line = Console.ReadLine()?.Trim();
+			if (line == null)
+			{
+				break;
+			}
+			
+			if (line == "") // Empty lines delimiter the elves
+			{
+				all_elves.Add(total_calories);
+				total_calories = 0;
+			}
+			else
+			{
+				if (int.TryParse(line, out int calorie))
+				{
+					total_calories += calorie;
+				}
+				else
+				{
+					Console.Error.WriteLine($"Invalid calorie {{{line}}}");
+				}
+			}
+		}
+		
+		if (all_elves.Count > 0)
+		{
+			var top_elves = all_elves
+				.OrderByDescending(x=>x)
+				.Take(3);
+			
+			Console.WriteLine($"The top elf carries {top_elves.First()}");
+			Console.WriteLine($"The top 3 elves carry {top_elves.Sum()}");
+		}
+		else
+		{
+			Console.WriteLine("No elves were found.");
+		}
+	}
+}
